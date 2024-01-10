80aceaf2eed2d1c2f2f36bfe44ad7610d51bc6c9
ninggf <windywany@163.com>
2024-01-10 10:36:49 +0800

    move core and login module to its repository

--- src/modules/core/router.ts
+++
@@ -1,9 +0,0 @@
-import type { RouteRecordRaw } from 'vue-router'
-
-export default [
-  {
-    path: '/',
-    name: 'index',
-    component: () => import('~/core/views/Index.vue')
-  }
-] satisfies RouteRecordRaw[]
