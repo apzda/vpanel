10cb95e25dcbcaea458bceb42b5b83170d2bfa80
ninggf <windywany@gmail.com>
2024-06-20 15:40:25 +0800

    新增网络请求封装

--- src/config/handler.ts
+++ src/config/handler.ts
@@ -1,7 +1,21 @@
+import { v4 as uuid_v4 } from 'uuid'
+
 import { gotoLoginPage, gotoPage } from '@/router'
 import { defineHandler, type RequestConfig } from '@/@types'
-import type { CommonResponse } from '@/@types/request'
+import type { CommonResponse, ErrorEvent } from '@/@types/request'
+import { language } from '@/utils/lang'
 import { msgBox } from '@/utils/msgbox'
+
+import settings from '@/config/settings'
+import { user } from '@/stores/user'
+
+// 持票人
+const bearer = settings.tokenBearer ? settings.tokenBearer + ' ' : ''
+
+// 刷新AccessToken
+const refreshAccessToken = (event: ErrorEvent) => {
+  // todo 刷新AccessToken吧
+}
 
 export default defineHandler({
   onErr401({ url, next }) {
@@ -22,7 +36,7 @@ export default defineHandler({
   },
   onErr810(event) {
     event.suppress = true
-    console.error('AccessToken 过期啦')
+    refreshAccessToken(event)
     return true
   },
   onErr813(event) {
@@ -31,6 +45,26 @@ export default defineHandler({
     return true
   },
   beforeRequest(options: RequestConfig): RequestConfig {
+    options.headers = options.headers || {}
+
+    if (!user.value.uuid) {
+      user.value.uuid = uuid_v4()
+    }
+    options.headers['uuid'] = user.value.uuid
+
+    if (settings.tokenHeaderName && user.value.accessToken && options.login !== false) {
+      options.headers[settings.tokenHeaderName] = bearer + user.value.accessToken
+    }
+
+    options.headers['Accept-Language'] = language.value
+
+    if (options.method?.toLowerCase() == 'get') {
+      options.params = {
+        _t: new Date().getTime(),
+        ...options.params
+      }
+    }
+
     return options
   },
   showTipMessage(success: boolean, type, message: string) {
