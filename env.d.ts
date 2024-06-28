74f9222954b373ee62e2ae4ffd4029a8e0e7dbe0
ninggf <windywany@gmail.com>
2024-06-28 18:35:16 +0800

    加解密工具

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,10 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_NAME: string
+  readonly VITE_ENC_KEY: string;
+  readonly VITE_ENC_IV: string;
+  readonly VITE_ENC_ALG: 'aes' | 'des' | 'sm4';
+  readonly VITE_GTW_DEFAULT_URL: string;
 }
 
 interface ImportMeta {
