a01509aee7b976baea1304b0798db82679839cac
ninggf <windywany@gmail.com>
2024-08-16 18:51:44 +0800

    优化配置

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
 import type { Config } from 'tailwindcss'
 
-export default {
+export default <Config>{
   darkMode: 'class',
   content: ['./index.html', '!./src/**/__tests__/*', './src/**/*.{vue,ts,tsx}'],
   theme: {
     extend: {}
   },
   plugins: []
-} satisfies Config
+}
